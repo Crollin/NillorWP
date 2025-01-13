052977d0fa25da9b77d8f7586be79e5a477ad4cc
Crollin <dev@creactiveweb.com>
2025-01-13 15:06:19 +0100

    Update - 2.3.10
    * Suppression logs en-tête

--- inc/class-init.php
+++ inc/class-init.php
@@ -9,7 +9,6 @@ class Init {
         AdminSettings::register();
 
         $options = get_option('creactive_settings');
-        var_dump(get_option('creactive_settings'));
 
         // SKU Search
         if (! empty($options['enable_feature_sku_search'])) {
